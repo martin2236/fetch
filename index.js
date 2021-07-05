9bba09f63f8d99a009f4eb2ce959e839510a3318
Martin <martoxxx100@gmail.com>
2021-07-05 19:48:05 -0300

    terminado

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const fragment = document.createDocumentFragment()
+
+function mostrarResultados(res,total){
+    console.log(total)
+    const busquedas = document.querySelector(".busquedas")
+    const contenedor = document.querySelector(".contenedor")
+    contenedor.innerHTML=""
+    const template = document.querySelector(".template").content
+     res.forEach(item=>{
+        template.querySelector("h4").textContent = item.title
+        template.querySelectorAll("span")[1].textContent =item.price
+        template.querySelector("span").textContent = item.sold_quantity
+        template.querySelector("img").setAttribute("src",item.thumbnail)
+        const clone = template.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+   contenedor.appendChild(fragment)
+   busquedas.textContent = total
+}
 
 
 function main(){
@@ -10,10 +29,15 @@ formEl.addEventListener("submit",(e)=>{
 
     fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + queBuscar).then((res)=>{
     return res.json()
-}).then((res2)=>{
-    console.log(res2)
+}).then((data)=>{
+    
+    mostrarResultados(data.results,data.paging.total)
 })
 })
+
+
+
+
 
 }
 main()
